6b1a43924cf6293437458aca91b14bd8b581fa54
Nihal Ramteke <ramteke.nihal@gmail.com>
2020-08-01 10:09:12 +0530

    correcting varibale spell mistake

--- cachematrix.R
+++ cachematrix.R
@@ -15,20 +15,8 @@
 
 ##get the value of the inverse
 
-makeCacheMatrix <- function(x = matrix()) {
-  i <- NULL
-  set <- function(y) {
-    x <<- y
-    i <<- NULL
-  }
-  get <- function() x
-  setinverse <- function(inverse) i <<- inverse
-  getinverse <- function() i
-  list(set = set,
-       get = get,
-       setinverse = setinverse,
-       getinverse = getinverse)
-}
+
+
 
 
 ## This function computes the inverse of the special “matrix” returned by makeCacheMatrix above. 
