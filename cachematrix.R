ece757d73bd771877a88335f5d6b9a9090a1db51
Nihal Ramteke <ramteke.nihal@gmail.com>
2020-08-01 10:10:52 +0530

    Resubmission

--- cachematrix.R
+++ cachematrix.R
@@ -15,8 +15,20 @@
 
 ##get the value of the inverse
 
-
-
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
 
 
 ## This function computes the inverse of the special “matrix” returned by makeCacheMatrix above. 
